d70bd4f9438e9b37f1f1e65ee03ec823652c82c3
Alaybey <alaybey@users.noreply.github.com>
2018-10-26 12:03:45 +0300

    Update Main.java
    Version 0.1.1 published

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,13 @@
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 
+public static final String COFFEE ="Kahve ";
+
 public class Main {
 
     public static void main(String[] args) throws FileNotFoundException {
         ArrayList<String> names = new ArrayList<>();
         names = new add().run(names);
-        System.out.println("Kahve " + new generate().run(names));
+        System.out.println(COFFEE + new generate().run(names));
     }
 }
